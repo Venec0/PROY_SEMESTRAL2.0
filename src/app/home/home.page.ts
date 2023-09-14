87185e0ddd1e1ec3d3b0eaee6fb816433370d2e5
Venec0 <joeljesus30030@gmail.com>
2023-09-14 14:09:17 -0300

    COMMIT, EN BUSCA DE CORRECCIÓN DE LOGIN

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,4 +10,7 @@ export class HomePage {
 
   constructor() {}
 
+}
+export class LoginForm {
+  showLoginForm = false; // Inicialmente, el formulario no se muestra
 }
