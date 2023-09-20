2e6f35bfd22ab47373171ba0656e1b93c38ff856
Venec0 <joeljesus30030@gmail.com>
2023-09-20 19:09:22 -0300

    3ER PUSH A 2DA RAMA

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -27,7 +27,6 @@ export class HomePage {
     if (user && user.password === this.password) {
       const welcomeMessage = `Bienvenido, ${user.name}!`;
 
-      // Muestra un alert con el mensaje de bienvenida
       const alert = await this.alertController.create({
         header: 'Inicio de sesión exitoso',
         message: welcomeMessage,
@@ -35,7 +34,6 @@ export class HomePage {
           {
             text: 'OK',
             handler: () => {
-              // Redirige al usuario a la página de "login" con un mensaje en la URL
               this.router.navigate(['/login'], {
                 queryParams: { message: `Ingresado como ${user.name}` },
               });
@@ -48,7 +46,6 @@ export class HomePage {
     } else {
       const errorMessage = 'Credenciales incorrectas';
 
-      // Muestra un alert con el mensaje de error
       const alert = await this.alertController.create({
         header: 'Error',
         message: errorMessage,
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,31 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  welcomeMessage: string = ''; // Variable para almacenar el mensaje de bienvenida
+  welcomeMessage: string = '';
+  aulas: any[] = [
+    { id: 1, codigo: '12345', qrCodeImage: 'assets/icon/qr-code.png' },
+    { id: 2, codigo: '67890', qrCodeImage: 'assets/icon/qr-code.png' },
+    { id: 3, codigo: '54321', qrCodeImage: 'assets/icon/qr-code.png' },
+  ];
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private modalController: ModalController
+  ) {}
 
   ngOnInit() {
-    // Obtén el mensaje de bienvenida de los parámetros de la URL
     this.activatedRoute.queryParams.subscribe((params) => {
-      this.welcomeMessage = params['message'] || ''; // Mensaje predeterminado vacío si no se proporciona
+      this.welcomeMessage = params['message'] || '';
     });
+  }
+
+  mostrarCodigoQR(qrCodeImage: string) {
+    // Puedes mostrar la imagen del código QR en una vista modal o en una nueva página.
+    // Aquí se muestra en una vista modal como ejemplo.
+    const modal = document.createElement('ion-modal');
+    modal.component = 'app-qr-modal'; // Crea un componente para mostrar la imagen del código QR
+    modal.componentProps = { qrCodeImage };
+    document.body.appendChild(modal);
+    return modal.present();
   }
 }
